4665e78f6d55da2e6b69e4e43d564ee281ef65f1
Jeremy Albright <myrlin1@gmail.com>
2020-02-08 14:06:32 -1000

    didn't work

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,20 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-// const fs = require('fs');
-
-// exports.onCreatePage = async ({ page, actions }) => {
-    // const { deletePage, createPage } = actions;
-    // const pageModule = await import(page.componentPath);
-    // const oldPage = {
-    //     ...page,
-    // }
-    // if (pageModule.context) {
-    //     page.context = {
-    //         ...page.context,
-    //         ...pageModule.context,
-    //     }
-    //     deletePage(oldPage);
-    //     createPage(page);
-    // }
-// }
